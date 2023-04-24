13bfe4a8fa3facdb4e3b41f449d63cb777f8a4b6
StasSova <112925204+StasSova@users.noreply.github.com>
2023-04-24 12:07:18 +0300

    ...

---
+++ HW12_SearchInFiles_Mutex/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace HW12_SearchInFiles_Mutex
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(694, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Поиск";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(597, 32);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Обзор";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(286, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(305, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 32);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(239, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Что искать";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(283, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(185, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "В какой директории искать";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(16, 92);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(753, 340);
+            this.listBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ HW12_SearchInFiles_Mutex/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HW12_SearchInFiles_Mutex
+{
+    public partial class Form1 : Form
+    {
+        Mutex mutex;
+        SynchronizationContext uiContext;
+        public Form1()
+        {
+            InitializeComponent();
+            mutex = new Mutex(false,"My_Mutex");
+            uiContext = SynchronizationContext.Current;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.OK == folderBrowserDialog1.ShowDialog())
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            uiContext.Send(d => listBox1.Items.Clear(), null);
+            List<string> PathFiles = new List<string>();
+            // получаю список всех файлов
+            PathFiles.AddRange(Directory.GetFiles(textBox1.Text,"*.txt",SearchOption.AllDirectories));
+            string search = textBox2.Text;
+            foreach (string path in PathFiles)
+            {
+                await Task.Run(() => {
+                    int repeat = 0;
+                    using (StreamReader sr = new StreamReader(path, Encoding.UTF8))
+                    {
+                        string text = sr.ReadToEnd();
+                        int pos = 0;
+                        while ((pos = text.IndexOf(search, pos)) != -1)
+                        {
+                            repeat++;
+                            pos += search.Length;
+                        }
+                    }
+                    if (repeat > 0)
+                    {
+                        mutex.WaitOne();
+                        uiContext.Send(d=>listBox1.Items.Add("-----------------------------------------"),null);
+                        uiContext.Send(d => listBox1.Items.Add($"File name: {Path.GetFileName(path)}"), null);
+                        uiContext.Send(d => listBox1.Items.Add($"Path: {path}"), null);
+                        uiContext.Send(d => listBox1.Items.Add($"Repeat: {repeat}"), null);
+                        uiContext.Send(d => listBox1.Items.Add("-----------------------------------------"), null);
+                        mutex.ReleaseMutex();
+                    }
+                });
+            }
+        }
+    }
+}
